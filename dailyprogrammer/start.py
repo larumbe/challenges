3f5c0530e766598ecf3bfeefffeb5f49b2063758
Hadrian Larumbe <adrianml@alumnos.upm.es>
2017-05-19 01:39:17 +0200

    Added solution for 314 easy daily programmer

--- dailyprogrammer/start.py
+++ dailyprogrammer/start.py
@@ -2,44 +2,28 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/69y21t/20170508_challenge_314_easy_concatenated_integers/
 # http://www.shiftedup.com/2015/05/07/five-programming-problems-every-software-engineer-should-be-able-to-solve-in-less-than-1-hour
 
+# DONE
+
 import functools
 
+def probsort(a,b):
+    if (len(str(a)) == len(str(b))):
+        return a - b
+    c , d = max(a,b), min(a,b)
+    if (str(c)[:len(str(d))] == str(d)):
+        if (str(c)[len(str(d))] > str(d)[0]):
+            if (c == b): return -1
+            else: return 1
+        else:
+            if (c == b): return 1
+            else: return -1
+    else:
+        minlen = len(str(min(a,b)))
+        return (int(str(a)[:minlen]) - int(str(b)[:minlen]))
 
-def sortnum(a,b):
-    # define a sorting function taking  digits into account
-    
-    
+def nextlargestnumber(numlist):
+    return int("".join([str(n) for n in sorted(numlist, key=functools.cmp_to_key(probsort), reverse=True)]))
 
-def longestvalue(l):
-    result = ''
-    while (len(l) > 0):
-        n = max([str(x) for x in l])
-        # how can you pass the sorting function to max?
-        del l[l.index(int(n))]
-        result += n
-    return int(result)
-        
-        
-
-
-# l = [53, 254, 2634]
-
-# max([str(x) for x in l])
-
-# n = max([str(x) for x in l])
-
-# l.index(53)
-
-# del l[l.index(53)]
-
-
-    
-
-    
-
-
-
-
-
-    
+def nextsmallestnumber(numlist):
+    return int("".join([str(n) for n in sorted(numlist, key=functools.cmp_to_key(probsort))]))
 

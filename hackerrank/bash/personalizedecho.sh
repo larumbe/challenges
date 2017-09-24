11c4f3a4d5213f47c913a723ba1cbaa72c3879b3
Hadrian Larumbe <adrianml@alumnos.upm.es>
2017-09-24 23:42:36 +0200

    Personalized echo bash challenge

---
+++ hackerrank/bash/personalizedecho.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Write a Bash script which accepts name as input and displays a greeting: "Welcome (name)"
+
+# Input from user
+
+read -p "Type your name: " name;
+echo "Welcome" $name
+

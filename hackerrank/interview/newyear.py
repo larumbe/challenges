9a4774583b157ef6c489bf59318a19eb6b01be1c
Hadrianus Larunbe <adrianml@alumnos.upm.es>
2018-09-12 18:07:29 +0100

    There's an error, for entry, not in the right place

--- hackerrank/interview/newyear.py
+++ hackerrank/interview/newyear.py
@@ -1,5 +1,10 @@
 import sys
 
+# the great bottle neck is the number of times everything has to be recomputed
+# use memoization
+
+d = dict()
+
 def minimumBribes(q):
     # start recursion
     res =  bribeRec(q, [0]*len(q))
@@ -13,6 +18,7 @@ def minimumBribes(q):
 def bribeRec(q, sw):
     # sw = swaps array
     # q = queue
+    global d
 
     # base case
     if (q == list(range(1,len(q)+1))):
@@ -33,18 +39,24 @@ def bribeRec(q, sw):
     
     for i in range(len(q)-1):
         if ((sw[q[i]-1]) < 2):
-            # generate local copies of q,sw for recursive call
-            tq = q[:]
-            ts = sw[:]
-            ts[tq[i]-1] += 1
-            tq[i], tq[i+1] = tq[i+1], tq[i]
-            res = bribeRec(tq, ts)
+            # check the table for intermediate results
+            entry = d.get(repr(q))
+            if (entry):
+                res = entry
+            else:
+                tq = q[:]
+                ts = sw[:]
+                ts[tq[i]-1] += 1
+                tq[i], tq[i+1] = tq[i+1], tq[i]
+                res = bribeRec(tq, ts)
             if (res != None):
                 allNone = False
                 if (res < localmin):
                     localmin = res
 
     if (not(allNone)):
+        # update the dictionary
+        d[repr(q)] = localmin
         return localmin
     else:
         return None

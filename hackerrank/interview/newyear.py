b41e912f783dbc887259890d75a3912e1645c1ae
Hadrianus Larunbe <adrianml@alumnos.upm.es>
2018-09-12 17:53:18 +0100

    Working newyear.py, but incredibly slow, needs improving

--- hackerrank/interview/newyear.py
+++ hackerrank/interview/newyear.py
@@ -1,45 +1,53 @@
+import sys
+
 def minimumBribes(q):
-    # swaps array
-    swv = [0] * len(q)
-    # non-local sorted queue for base case lookups
-    sq = list(range(1,len(q)+1))
-
-    # recursife function
-    def bribeRec(q, s):
-        
-        nonlocal sq
-
-        # base case
-        if (q == sq):
-            return sum(s)
-
-        # pruning
-        isSwap = False
-        for j in range(len(q)-1):
-            if ((s[q[j]-1]) < 2):
-                isSwap = True
-                break
-
-        if (not isSwap):
-            return 0
-
-        localmax = 0
-        for i in range(len(q)-1):
-            if ((s[q[i]-1]) < 2):  # there's a possible swap here
-                # generate local copies of q,s for recursive call
-                tq = q[:]
-                ts = s[:]
-                ts[tq[i]-1] += 1
-                tq[i], tq[i+1] = tq[i+1], tq[i]
-                res = bribeRec(tq, ts)
-                if (res > localmax):
-                    localmax = res
-        
-        return localmax        
-                
-    
     # start recursion
-    return bribeRec(q, swv)
+    res =  bribeRec(q, [0]*len(q))
+    if (res != None):
+        print("{}".format(res))
+    else:
+        print("Too chaotic")
+
+
+# recursife function
+def bribeRec(q, sw):
+    # sw = swaps array
+    # q = queue
+
+    # base case
+    if (q == list(range(1,len(q)+1))):
+        return sum(sw)
+
+    # pruning
+    isSwap = False
+    for j in range(len(q)-1):
+        if ((sw[q[j]-1]) < 2):
+            isSwap = True
+
+    if (not(isSwap)):
+        return None
+
+    # explore the graph
+    allNone = True
+    localmin = sys.maxsize
+    
+    for i in range(len(q)-1):
+        if ((sw[q[i]-1]) < 2):
+            # generate local copies of q,sw for recursive call
+            tq = q[:]
+            ts = sw[:]
+            ts[tq[i]-1] += 1
+            tq[i], tq[i+1] = tq[i+1], tq[i]
+            res = bribeRec(tq, ts)
+            if (res != None):
+                allNone = False
+                if (res < localmin):
+                    localmin = res
+
+    if (not(allNone)):
+        return localmin
+    else:
+        return None
 
 
 

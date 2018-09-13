3cf64d73953d10b9ef9ac552d17085e0a0d5b63c
Hadrianus Larunbe <adrianml@alumnos.upm.es>
2018-09-13 01:10:25 +0100

    Second version of minimumswaps, works

--- hackerrank/interview/minimumswaps2.py
+++ hackerrank/interview/minimumswaps2.py
@@ -3,16 +3,13 @@
 # position in the array
 
 def minimumSwaps(arr):
-    i = 0;
     swaps = 0
-
-    while (i < (len(arr)-1)):
+    
+    for i in range(len(arr)):
         while (arr[i] != i+1):
             temp = arr[i]
             arr[i] = arr[temp-1]
             arr[temp-1] = temp
             swaps += 1
-            print(arr)
-        i += 1
-
+            
     return swaps

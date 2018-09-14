818f306f7e640a7829eee92c7124435e94f4419f
Hadrianus Larunbe <adrianml@alumnos.upm.es>
2018-09-14 17:33:58 +0100

    list exercises

---
+++ hackerrank/interview/linkedlists/insertnode.py
@@ -0,0 +1,56 @@
+# https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def insertNodeAtPosition(head, data, position):
+    if (head is None):
+        first = SinglyLinkedListNode(data)
+        return first
+    
+    current = head
+    
+    for i in range(position-1):
+        if (current.next is None):
+            return head
+        current = current.next
+
+    newnode = SinglyLinkedListNode(data)
+    newnode.next = current.next
+    current.next = newnode
+
+    return head
